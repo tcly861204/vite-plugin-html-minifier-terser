aee48d8866ecb782dadac0c40cd8a1c5f9cfb8a8
tcly861204 <tcly861204@hotmail.com>
2023-07-18 06:43:12 +0800

    fix: 🐛 完善前端

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Plugin, PluginOption } from 'vite';
-interface VitePluginHtmlMinifierTerser {
-  removeAttributeQuotes?: boolean,
-  collapseWhitespace?: boolean,
-}
-export default function vitePluginHtmlMinifierTerser(options?: VitePluginHtmlMinifierTerser):Plugin | PluginOption;
